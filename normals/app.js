664d4fb397545b3db2f254142862f2cab92f42ea
George Bazerashvili <georgebazerashvili200@gmail.com>
2022-12-29 18:27:53 +0400

    conections added

---
+++ normals/app.js
@@ -0,0 +1,12 @@
+const stageOne = 1500;
+const stageTwo = 1200;
+const stageThree = 1000;
+const stageFour = 700;
+const stageFive = 400;
+
+const time = document.getElementById("time");
+const input = document.getElementById("input");
+const words = document.getElementById("words");
+
+let score = document.getElementById("score");
+let stage = document.getElementById("stage");
