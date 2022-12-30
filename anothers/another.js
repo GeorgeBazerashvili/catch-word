c4d268852921f4e57310e6e40f379e787a85d760
GeorgeBazerashvili <110630764+GeorgeBazerashvili@users.noreply.github.com>
2022-12-30 13:56:49 +0400

    Delete anothers directory

--- anothers/another.js
+++
@@ -1,8 +0,0 @@
-const description = document.getElementById("button");
-const section = document.getElementById("section");
-const actualDescription = document.getElementById("description");
-
-description.addEventListener("click", () => {
-  section.style.visibility = "hidden";
-  actualDescription.style.visibility = "visible";
-});
